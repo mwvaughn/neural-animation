8672a828a44e6dfe426e633ed66fcb18aaf17203
Matthew Vaughn <vaughn@tacc.utexas.edu>
2016-03-14 20:27:45 -0500

    Proof of concept threaded implementation

--- paint.py
+++ paint.py
@@ -7,40 +7,25 @@ import sys
 import os
 import errno
 import time
+from random import randint
 
-import Queue
-from threading import Thread
+from multiprocessing import Queue, Process
+#from Queue import Queue
+#from threading import Thread
 
-gpus = 4
 extra_arguments = "-cudnn_autotune -backend cudnn -image_size 512"
+
 # How many GPUs on system by default
-render_queue = []
-for z in range[ (gpus - 1) ]:
-    q = Queue()
-    render_queue.append[q]
+gpus = 4
 
-def render(i, q):
-    """This is the worker thread function.
-    It processes items in the queue one after
-    another.  These daemon threads go into an
-    infinite loop, and only exit when
-    the main thread ends.
-    """
-    while True:
-        print '%s: Looking for the next render job' % i
-        command = q.get()
-        print 'Rendering ' + command
-        # instead of really rendering
-        # we just pretend and sleep
-        time.sleep(i + 2)
-        #os.system( command )
-        q.task_done()
 
-# Set up some threads to render frames
-for i in range( (gpus - 1) ):
-    worker = Thread(target=render, args=(i, render_queue[i],))
-    worker.setDaemon(True)
-    worker.start()
+def render_worker( work_queue, done_queue ):
+
+    for cmd in iter( work_queue.get, 'STOP' ):
+        print 'Rendering ' + cmd
+        time.sleep(randint(1,5))
+        print 'Rendered'
+        done_queue.put('Success: ' + cmd)
 
 def make_sure_path_exists(path):
     '''
@@ -54,9 +39,19 @@ def make_sure_path_exists(path):
             raise
 
 def main(input, output, style, start_frame, end_frame, gpus, extra_arguments):
+
     make_sure_path_exists(input)
     make_sure_path_exists(output)
 
+    render_queue = []
+    for z in range( gpus ):
+        q = Queue()
+        render_queue.append(q)
+
+    done_queue = Queue()
+    processes = []
+
+# Figure out frame count
     nrframes =len([name for name in os.listdir(input) if os.path.isfile(os.path.join(input, name))])
     if nrframes == 0:
         print("Found no frames to process...")
@@ -71,22 +66,36 @@ def main(input, output, style, start_frame, end_frame, gpus, extra_arguments):
     else:
 	nrframes = nrframes+1
 
-    # Cycle through GPUs, adding to queue
-    # Risks leaving GPUs underutilized towards end of job list
-    for i in xrange(frame_i, nrframes, num_gpus):
-        for g in range(0, num_gpus - 1):
-            frame_j = frame_i + g
+# Submit tasks
+    for i in xrange(frame_i, nrframes, gpus):
+        for g in range(gpus):
+            frame_j = i + g
+            #print 'frame_j ' + str(frame_j)
+
             if frame_j <= nrframes:
-                print 'Queueing on GPU ' + g
-                print 'Frame #{}'.format(frame_j)
-                command = "th neural_style.lua " + extra_arguments + "-gpu " + g + " -num_iterations 1000 -style_image " + style + " -content_image " + input + "/%08d.jpg" % frame_j + " -save_iter 2000 -output_image " + output + "/%08d.jpg" % frame_j
+                print 'Frame: {}'.format(frame_j)
+                print 'GPU: ' + str(g)
+                command = "th neural_style.lua " + extra_arguments + "-gpu " + str(g) + " -num_iterations 1000 -style_image " + style + " -content_image " + input + "/%08d.jpg" % frame_j + " -save_iter 2000 -output_image " + output + "/%08d.jpg" % frame_j
                 render_queue[g].put(command)
+
+# Start worker processes
+    for w in xrange( gpus ):
+        Process(target=render_worker, args=(render_queue[w], done_queue)).start()
+
+# Print output queue
+    for i in xrange(nrframes):
+        print done_queue.get()
+
+# Send STOP
+    for zz in xrange( gpus ):
+        render_queue[zz].put('STOP')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Implementing neural art on video.')
     parser.add_argument(
         '-g','--gpus',
-        help='Number of host GPUs', type=int, default=1,
+        help='Number of host GPUs', type=int, default=4,
         required=False)
     parser.add_argument(
         '-x','--extra_arguments',
@@ -117,4 +126,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(args.input, args.output, args.style, args.start_frame, args.end_frame, args.gpu, args.extra_arguments)
+    main(args.input, args.output, args.style, args.start_frame, args.end_frame, args.gpus, args.extra_arguments)
